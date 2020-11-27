cd9a336a195149e325ad90e3ea85923970307d35
Kerolayne Souza <kerolaynesouza@outlook.com.br>
2020-11-26 21:20:36 -0300

    Update teg_teste-1.py

--- teg_teste-1.py
+++ teg_teste-1.py
@@ -1,25 +1,41 @@
 
-def criaMatriz(n_lin, n_col, n):
+
+def criaMatriz(n_vertices, n):
     matriz=[]
 
-    for i in range(n_lin):
+    for i in range(n_vertices):
         linha = []
-        for i in range(n_col):
+        for i in range(n_vertices):
             linha += [n]
 
         matriz += [linha]
 
     return matriz
 
-#def leMatriz(pos_l, ):
+def inserePosMatriz(linha, coluna, n_vertices):
+    i = 0
+    for i in range(n_vertices):
+        if i == linha:
+            for i in range(n_vertices):
+                if i == coluna:
+                    linha += 1
+
+    return matriz
+
 
 def main():
-    n_vertices = input("digite o numero de linhas: ")
+    n_vertices = input("digite o numero de vertices: ")
    
     n = input("preencher a matriz com qual número?  ")
 
-    matriz = criaMatriz(int(n_vertices), int(n_vertices), int(n))
+    matriz = criaMatriz(int(n_vertices), int(n))
     print(matriz)
+
+    linha = input("vertice 1: ")
+    coluna = input("vertice 2: ")
+
+    matrizA = inserePosMatriz(int(linha), int(coluna), int(n), matriz)
+    print(matrizA)
     
 if __name__ == "__main__":
     main()
